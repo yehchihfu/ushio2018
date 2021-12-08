1262b6d49d6b58a4654bcd5831c336431cb4ed30
Maike Morrison <maike.morrison@gmail.com>
2021-12-08 11:25:01 -0800

    Add Roxygen comments to plot_time_3d function

--- R/plot_function.R
+++ R/plot_function.R
@@ -1,5 +1,8 @@
-
-plot = function(int_time_series){
+# plot_time_3d -----------------------------
+#' Generate a plot of species interactions over time
+#' @param int_time_series A data frame is the output of the function `int_extract` with columns `species_to_species` (characters naming species interaction) `time` (observations of time points), and `strength` (the strength of the species interaction).
+#' @return A plot_ly 3D object
+plot_time_3d <- function(int_time_series){
   level <- unique(int_time_series$species_to_species)
 
   df <- int_time_series %>%
@@ -9,7 +12,6 @@ plot = function(int_time_series){
            time = (int_time_series$time * 14)/365 + 2002 + 157/365)
 
   plot_ly(df, y =~time , z = ~strength, x =~species_to_species ) %>%
-    # group_by(species_to_species) %>%
     add_lines(color = ~species_to_species) %>%
     layout(scene = list(yaxis = list(title = "Sample Time"),
                         zaxis = list(title = "Interaction Strength"),
