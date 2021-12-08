750ad4da8a955f4127bc6aed9dffafad4cb50783
Maike Morrison <maike.morrison@gmail.com>
2021-12-08 11:17:13 -0800

    Update plot function: make ordered factor

--- R/plot_function.R
+++ R/plot_function.R
@@ -1,6 +1,18 @@
-plot = function(){
 
-  plot_ly(int_time_series, x =~time , y = ~strength, z =~species_to_species ) %>%
-    group_by(species_to_species) %>%
-    add_lines(color = ~species_to_species)
+plot = function(int_time_series){
+  level <- unique(int_time_series$species_to_species)
+
+  df <- int_time_series %>%
+    mutate(species_to_species = factor(species_to_species,
+                                       ordered = TRUE,
+                                       levels = level),
+           time = (int_time_series$time * 14)/365 + 2002 + 157/365)
+
+  plot_ly(df, y =~time , z = ~strength, x =~species_to_species ) %>%
+    # group_by(species_to_species) %>%
+    add_lines(color = ~species_to_species) %>%
+    layout(scene = list(yaxis = list(title = "Sample Time"),
+                        zaxis = list(title = "Interaction Strength"),
+                        xaxis = list(title = "Species Interactions",
+                                     showticklabels = FALSE)))
 }
