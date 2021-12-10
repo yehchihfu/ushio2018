68b7617f2dc91cacbd1205350adf231337731bab
chihfu <yehcf@stanford.edu>
2021-12-10 10:43:53 -0800

    update discription and zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,12 @@
 .onLoad <- function(libname, pkgname) {
+  if (packageVersion("rEDM") > "0.4.4") {
+    msg <- paste("ushio2018 requires rEDM <= 0.4.4. You have ", packageVersion("rEDM"))
+    packageStartupMessage(msg)
+  }
+
+  if (packageVersion("plotly") < "4.10.0") {
+    msg <- paste("ushio2018 requires plotly >= 4.10.0. You have ", packageVersion("plotly"))
+    packageStartupMessage(msg)
+  }
   reticulate::configure_environment(pkgname)
 }
